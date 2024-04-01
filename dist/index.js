a2586860865e14b2b8029fd438ea3342327f55c7
S. David Kyle <sdavidkyle@gmail.com>
2024-03-31 20:06:05 -0700

    Small changes to preogram.cs

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,3 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === "number") {
-        return weight * 2.20462;
-    }
-    else {
-        return parseInt(weight) * 2.20462;
-    }
-}
+let quantity = 50;
 //# sourceMappingURL=index.js.map
