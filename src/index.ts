952200bd05e5d62e123ee673480f541622834af1
S. David Kyle <sdavidkyle@gmail.com>
2024-04-09 16:28:37 -0700

    Generics

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,5 @@
-// abstract class Calendar {
-//   constructor(public name: string) {}
-
-//   abstract addEvent(): void;
-//   abstract removeEvent(): void;
-// }
-interface ICalendar {
-  name: string;
-  addEvent(): void;
-  removeEvent(): void;
-  address: {
-    street: string;
-    city: string;
-  };
+class KeyValuePair<T, U> {
+  constructor(public key: T, public value: U) {}
 }
 
-interface ICloudCalendar extends ICalendar {
-  sync(): void;
-}
-
-class GoogleCalendar implements ICloudCalendar {
-  constructor(
-    public name: string,
-    public address: { street: string; city: string }
-  ) {}
-
-  addEvent() {
-    console.log("GoogleCalendar addEvent");
-  }
-
-  removeEvent() {
-    console.log("GoogleCalendar removeEvent");
-  }
-
-  sync() {
-    console.log("GoogleCalendar sync");
-  }
-}
+let pair = new KeyValuePair("1", "First");
