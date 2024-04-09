ef6353f0f61753294c8d39c50ea0dd49e992a7f7
S. David Kyle <sdavidkyle@gmail.com>
2024-04-08 18:49:24 -0700

    Index modifiers

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,7 @@
 "use strict";
-class Account {
-    constructor(id, owner, _balance) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = _balance;
-    }
-    deposit(amount) {
-        if (amount <= 0)
-            throw new Error("Deposit amount has to be greater than 0");
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
+class SeatAssignment {
 }
-let account = new Account(1, "John Doe", 0);
-account.deposit(100);
-console.log(account instanceof Account);
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "Steve";
+seats["A2"] = "Bob";
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,7 @@
-class Account {
-  nickname?: string;
-
-  constructor(
-    public readonly id: number,
-    public owner: string,
-    private _balance: number
-  ) {}
-
-  deposit(amount: number): void {
-    if (amount <= 0) throw new Error("Deposit amount has to be greater than 0");
-
-    this._balance += amount;
-  }
-
-  get balance(): number {
-    return this._balance;
-  }
-  set balance(value: number) {
-    this._balance += value;
-  }
+class SeatAssignment {
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "John Doe", 0);
-account.balance = 100;
-
-account.deposit(100);
-
-console.log(account instanceof Account);
-
-console.log(account.balance);
+let seats = new SeatAssignment();
+seats.A1 = "Steve";
+seats["A2"] = "Bob";
