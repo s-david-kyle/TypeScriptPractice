66f4293f227b2e06fab796efb48306163ff07264
S. David Kyle <sdavidkyle@gmail.com>
2024-04-15 15:33:31 -0700

    Decorators

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,20 @@
 "use strict";
-class Store {
-    constructor() {
-        this.products = [];
-    }
-    addProduct(product) {
-        this.products.push(product);
-    }
-    getProducts() {
-        return this.products;
-    }
-    find(property, value) {
-        return this.products.find((product) => product[property] === value);
-    }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Component(constructor) {
+    console.log("Component decorator called ");
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDom = () => {
+        console.log("Inserting into DOM");
+    };
 }
-let store = new Store();
-store.addProduct({ name: "Apple", price: 5 });
-store.find("name", "Apple");
+let ProfileComponent = class ProfileComponent {
+};
+ProfileComponent = __decorate([
+    Component
+], ProfileComponent);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-function echo<T>(arg: T): T {
-  return arg;
+function Component(constructor: Function) {
+  console.log("Component decorator called ");
+  constructor.prototype.uniqueId = Date.now();
+  constructor.prototype.insertInDom = () => {
+    console.log("Inserting into DOM");
+  };
 }
+
+@Component
+class ProfileComponent {}
