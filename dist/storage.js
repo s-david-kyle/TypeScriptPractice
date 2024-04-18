c16258e603f60df284a0665857cd779783fad650
S. David Kyle <sdavidkyle@gmail.com>
2024-04-17 19:22:01 -0700

    Wildcard imports

---
+++ dist/storage.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Format = void 0;
+class Store {
+}
+exports.default = Store;
+var Format;
+(function (Format) {
+    Format[Format["Raw"] = 0] = "Raw";
+    Format[Format["Compressed"] = 1] = "Compressed";
+    Format[Format["Encrypted"] = 2] = "Encrypted";
+})(Format || (exports.Format = Format = {}));
+class Compressor {
+}
+class Encryptor {
+}
+//# sourceMappingURL=storage.js.map
