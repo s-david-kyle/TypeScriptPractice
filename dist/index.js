9c84ccd52a63f38ef1b9667e5f7d41a6c6ca9987
S. David Kyle <sdavidkyle@gmail.com>
2024-04-18 15:41:07 -0700

    TypeScript with javascript using jsdocs

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const tax_1 = require("./tax");
+const taxValue = (0, tax_1.calculateTax)(100);
+console.log(taxValue);
+//# sourceMappingURL=index.js.map
---
+++ src/tax.js
@@ -0,0 +1,9 @@
+/**
+ * Calculate income tax
+ * @param {number} income - Net salary
+ * @returns {number}
+ */
+
+export function calculateTax(income) {
+  return income * 0.3;
+}
