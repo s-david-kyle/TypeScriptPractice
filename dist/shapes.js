785c6d29e2c6831d62e704029a6854c161d553b2
S. David Kyle <sdavidkyle@gmail.com>
2024-04-17 19:21:46 -0700

    Wildcard imports

--- dist/shapes.js
+++ dist/shapes.js
@@ -1,11 +1,16 @@
-export class Circle {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Square = exports.Circle = void 0;
+class Circle {
     constructor(radius) {
         this.radius = radius;
     }
 }
-export class Square {
+exports.Circle = Circle;
+class Square {
     constructor(side) {
         this.side = side;
     }
 }
+exports.Square = Square;
 //# sourceMappingURL=shapes.js.map
